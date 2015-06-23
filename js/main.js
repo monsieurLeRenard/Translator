4397e68fe1e821e8d8b2f8e243e61358d5896c8c
monsieurLeRenard <daniele.tabanella@photobox.com>
2015-06-23 20:15:40 +0200

    first function of translator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+var translator = {};
 
+$(function () {
+  // Handler for .ready() called.
+
+  var $in = $('#in'),
+    $out = $('#out'),
+    $go = $('#go');
+
+  $in.val('{"deco": "20% de réduction","print": "20% de réduction","wall_deco": "20% de réduction"}'); // fake
+
+
+  translator = {
+
+    process: function (input) {
+
+      console.log(typeof input, input);
+
+      var jsonInput = JSON.parse(input);
+
+      var output = '';
+
+      for (var key in jsonInput) {
+
+        if (jsonInput.hasOwnProperty(key)) {
+          var value = jsonInput[key];
+          console.log(key, value);
+        }
+        output += key + ' => "' + value + '",';
+
+      }
+
+      console.log(typeof jsonInput, jsonInput);
+
+      return output;
+    }
+
+  };
+
+  $go.click(function () {
+
+    var translatedText = translator.process($in.val());
+    $out.val(translatedText);
+  });
+
+});
---
+++ js/simple.js
@@ -0,0 +1,25 @@
+/*
+
+my $promo_sdp = {
+ book =>  q|20% de réduction|,
+ wall_deco => q|20% de réduction|,
+ print => q|20% de réduction|,
+ mug => q|20% de réduction|,
+ objet => q|20% de réduction|,
+ case => q|20% de réduction|,
+ clothing => q|20% de réduction|,
+ card => q|20% de réduction|,
+ calendar => q|20% de réduction|,
+ stationary => q|20% de réduction|,
+ pola => q|20% de réduction|,
+ perso => q|20% de réduction|,
+ deco => q|20% de réduction|,
+};
+
+*/
+
+var $promo_sdp = {
+ 'deco' : '20% de réduction',
+ 'print' : '20% de réduction',
+ 'wall_deco' : '20% de réduction'
+};
